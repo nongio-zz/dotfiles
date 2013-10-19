2bb3926c81c4091960edb5c66c74a69d372b95d0
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-10-19 14:54:21 +0200

    fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+### brew
+set PATH /usr/local/sbin $PATH
+### android
+set NDK /Users/riccardo/Lab/android/android-ndk-r8
+set PATH $PATH /Users/riccardo/Lab/android/android-ndk-r8
+set PATH $PATH:/Users/riccardo/Lab/android/android-ndk-r6/toolchains/arm-linux-androideabi-4.4.3/prebuilt/darwin-x86/bin/
+set PATH $PATH /Users/riccardo/Lab/android/android-sdk-macosx/tools
+set PATH $PATH /Users/riccardo/Lab/android/android-sdk-macosx/platform-tools
+
+### Add RVM to PATH for scripting
+set PATH /usr/local/bin:$PATH:$HOME/.rvm/bin 
+
+### npm
+set PATH $PATH:/usr/local/share/npm/bin/
+
+### Added by the Heroku Toolbelt
+set PATH /usr/local/heroku/bin $PATH
+set PATH /usr/local/bin $PATH
+
+
+alias ls='ls -G'
